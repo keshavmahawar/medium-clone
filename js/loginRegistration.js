bb9c5ca07bb29db9389a4d630c3608e4e358a342
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2020-07-16 02:05:56 +0530

    added login & signup functionality

--- js/loginRegistration.js
+++ js/loginRegistration.js
@@ -1,3 +1,31 @@
+import User from "./modules/userModule.js"
+
 window.onload = function() {
     $('#signIn').modal('show')
+    const logInBtn = document.querySelector("#logInBtn")
+    logInBtn.addEventListener('click', tryLogIn)
+    const signUpBtn = document.querySelector("#signUpBtn")
+    signUpBtn.addEventListener('click', createUser)
 }
+
+function tryLogIn() {
+    const userId = document.querySelector("#userIdL")
+    const password = document.querySelector("#passwordL")
+    const user = new User(userId.value, password.value)
+    user.login()
+    userId.value = ""
+    password.value = ""
+}
+
+function createUser() {
+    const nameC = document.querySelector("#nameC")
+    const userIdC = document.querySelector("#userIdC")
+    const passwordC = document.querySelector("#passwordC")
+    const user = new User(userIdC.value, passwordC.value, nameC.value)
+    user.create()
+    nameC.value = ""
+    userIdC.value = ""
+    passwordC.value = ""
+}
+
+
