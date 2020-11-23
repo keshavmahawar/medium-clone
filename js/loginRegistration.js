991074a460ca38b036e06904b8f3fb03d6f477ee
Keshav Mahawar <kes.mahawar@gmail.com>
2020-11-23 15:08:46 +0530

    location redirect after login fix

--- js/loginRegistration.js
+++ js/loginRegistration.js
@@ -1,36 +1,34 @@
-import User from "./modules/userModule.js"
+import User from "./modules/userModule.js";
 
-window.onload = function() {
-    if(User.checkLogin()) window.location = "index.html"
-    $('#signIn').modal('show')
-    const logInBtn = document.querySelector("#logInBtn")
-    logInBtn.addEventListener('click', tryLogIn)
-    const signUpBtn = document.querySelector("#signUpBtn")
-    signUpBtn.addEventListener('click', createUser)
-}
+window.onload = function () {
+    if (User.checkLogin()) window.location = "index.html";
+    $("#signIn").modal("show");
+    const logInBtn = document.querySelector("#logInBtn");
+    logInBtn.addEventListener("click", tryLogIn);
+    const signUpBtn = document.querySelector("#signUpBtn");
+    signUpBtn.addEventListener("click", createUser);
+};
 
 function tryLogIn() {
-    const userId = document.querySelector("#userIdL")
-    const password = document.querySelector("#passwordL")
-    const user = new User(userId.value, password.value)
+    const userId = document.querySelector("#userIdL");
+    const password = document.querySelector("#passwordL");
+    const user = new User(userId.value, password.value);
 
     if (user.login()) {
-        location.replace("/index.html")
+        location.replace("index.html");
     } else {
-        userId.style.color = "red"
-        password.style.color = "red"
+        userId.style.color = "red";
+        password.style.color = "red";
     }
 }
 
 function createUser() {
-    const nameC = document.querySelector("#nameC")
-    const userIdC = document.querySelector("#userIdC")
-    const passwordC = document.querySelector("#passwordC")
-    const user = new User(userIdC.value, passwordC.value, nameC.value)
-    user.create()
-    nameC.value = ""
-    userIdC.value = ""
-    passwordC.value = ""
+    const nameC = document.querySelector("#nameC");
+    const userIdC = document.querySelector("#userIdC");
+    const passwordC = document.querySelector("#passwordC");
+    const user = new User(userIdC.value, passwordC.value, nameC.value);
+    user.create();
+    nameC.value = "";
+    userIdC.value = "";
+    passwordC.value = "";
 }
-
-
